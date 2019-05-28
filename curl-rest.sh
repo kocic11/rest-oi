77fff3c0a88bd6d4d441b1490eb35bff7db61332
kocic11 <25288526+kocic11@users.noreply.github.com>
2019-05-28 11:46:17 -0400

    Updated

--- curl-rest.sh
+++ curl-rest.sh
@@ -2,27 +2,27 @@
 
 source ./env.properties
 # Retrieve Integrations
-curl --silen -u $user:$password -o integrations.json \
+curl --silent -u $user:$password -o integrations.json \
  -H "Accept: application/json" \
  -X GET \
  $server/ic/api/integration/v1/integrations
 
 # Retrieve Integration
-curl --silen -u $user:$password -o $id.json \
+curl --silent -u $user:$password -o $id.json \
  -H "Accept: application/json" \
  -X GET \
  $server/ic/api/integration/v1/integrations/$id 
 
  # Export an Integration
-curl --silen -u $user:$password -o $id.iar \
+curl --silent -u $user:$password -o $id.iar \
  -X GET \
  -H "Accept: application/octet-stream" \
  $server/ic/api/integration/v1/integrations/$id/archive
 
  # Retrieve and process Integrations
-curl --silen -u $user:$password -o ./integrations.json \
+curl --silent -u $user:$password -o ./integrations.json \
  -H "Accept: application/json" \
  -X GET \
- $server/ic/api/integration/v1/integrations | python3 parse.py ./integrations.json
+ $server/ic/api/integration/v1/integrations?orderBy=id | python3 parse.py ./integrations.json
 
 

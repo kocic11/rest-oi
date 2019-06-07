f9313cea0cb07b3b1928e94b11470dd31e63b703
kocic11 <25288526+kocic11@users.noreply.github.com>
2019-06-07 10:53:43 -0400

    Updated

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import os, requests, json
 
 def exportIntegration(baseUrl, id, auth):
     '''Export an integration with a given ID.'''
-    url = baseUrl + id + '/archive'
+    url = baseUrl + '/' + id + '/archive'
     response = requests.get(url, auth = auth, headers = {'Accept' : 'application/octet-stream'})
     if response.status_code == 200:
         fp = open(id + '.iar','wb')
@@ -20,42 +20,38 @@ def importIntegration(baseUrl, auth, file):
     response = requests.post(url, auth = auth, files = files)
     if response.status_code == 200:
         print('Integration added.')
-        exit()
+        return
 
     # Replace integration if it already exists
     if response.status_code == 409:
         files = {'file': open(file, 'rb')}
         response = requests.put(url, auth = auth, files = files)
-    
-    # Successful operation
-    if response.status_code == 204:
-        print('Integration replaced.')
-        exit()
+        # Successful operation
+        if response.status_code == 204:
+            print('Integration replaced.')
+            return
     
     if response.status_code == 400:
         print('No file is uploaded.')
         response.raise_for_status()
-        exit()
     
     if response.status_code == 404:
         print('Integration not found')
         response.raise_for_status()
-        exit()
     
     if response.status_code == 500:
         print('Server error.')
         response.raise_for_status()
-        exit()
     
 def activateIntegration(baseUrl, auth, id):
     '''Activate integration.'''
-    url = baseUrl + id
+    url = baseUrl + '/' + id
     payload = {'status':'ACTIVATED'}
     changeStatus(url, auth, payload)
 
 def deactivateIntegration(baseUrl, auth, id):
-    '''Activate integration.'''
-    url = baseUrl + id
+    '''Deactivate integration.'''
+    url = baseUrl + '/' + id
     payload = {'status':'CONFIGURED'}
     changeStatus(url, auth, payload)
 
@@ -64,15 +60,40 @@ def changeStatus(url, auth, payload):
     response = requests.post(url, auth = auth, headers = {'Content-Type' : 'application/json', 'X-HTTP-Method-Override' : 'PATCH'}, data = json.dumps(payload))
     if response.status_code == 200:
         print('Status changed.')
-        exit()
     else:
         response.raise_for_status()
 
 def retrieveConnections(baseUrl, auth):
-    ''' Retrieves a list of connections.'''
+    ''' Retrieve a list of connections.'''
     response = requests.get(baseUrl, auth = auth)
     if response.status_code == 200:
-        print(response.json())
-        exit()
+        connections = response.json()
     else:
         response.raise_for_status()
+
+def retrieveConnection(url, auth):
+    ''' Retrieve a connection.'''
+    response = requests.get(url, auth = auth, headers = {'Accept' : 'application/json'})
+    if response.status_code == 200:
+        return response.json()
+    
+    response.raise_for_status()
+
+def updateConnection(url, auth, name, payload):
+    ''' Update a connection.'''
+    url = url + '/' + name
+    response = requests.post(url, auth = auth, headers = {'X-HTTP-Method-Override' : 'PATCH', 'Content-Type' : 'application/json'}, data = json.dumps(payload))
+    if response.status_code in [200, 400]:
+        return
+    
+    response.raise_for_status()
+
+def uploadConnectionPropertyAttachment(url, auth, id, connPropName, file):
+    ''' Upload a connection property attachment.'''
+    url = url + '/' + id + '/attachments/' + connPropName
+    files = {'file': open(file, 'rb')}
+    response = requests.post(url, auth = auth, files = files)
+    if response.status_code == 200:
+        return
+    
+    response.raise_for_status()
--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import sys, json
 import os
 import requests
 from requests.exceptions import HTTPError
-from functions import exportIntegration, importIntegration, activateIntegration, deactivateIntegration, retrieveConnections
+from functions import *
 
 if (len(sys.argv) < 2) :
     print('Usage: parse.py <env.properties>')
@@ -13,32 +13,146 @@ fp = open(sys.argv[1], 'r')
 env = json.load(fp)
 fp.close()
 
-baseUrlInt = env['server'] + '/ic/api/integration/v1/integrations'
-baseUrlConn = env['server'] + '/ic/api/integration/v1/connections'
+source = env['source'] + env['integrations']
+target = env['target'] + env['integrations']
+sourceConnections = env['source'] + env['connections']
+targetConnections = env['target'] + env['connections']
 headers = {'Accept' : 'application/json'}
 auth = (env['user'], env['password'])
 
-ints = requests.get(baseUrlInt, auth = auth, headers = headers).json()
+# Get all integrations from source
+integrations = requests.get(source, auth = auth, headers = headers).json()
 
-print('\n')
-print('totalResults: ' + str(ints['totalResults']) + '\n')
-# for num in range (0, ints['totalResults']):
-#     id = (ints['items'][num]['id']).replace('|', '%7C')
-#     url = baseUrlInt + id
-#     response = requests.get(url, auth = auth, headers = headers)
-#     # if response.json()['status'] == 'ACTIVATED':
-#     print(str(num) + ': ' + id)
-#     exportIntegration(baseUrlInt, id, auth)
-try:
-    # importIntegration(baseUrlInt, auth, './EH_TEST%7C01.00.0000.iar')
-    # activateIntegration(baseUrlInt, auth, '/EH_TEST%7C01.00.0000')
-    # deactivateIntegration(baseUrlInt, auth, '/EH_TEST%7C01.00.0000')
-    retrieveConnections(baseUrlConn, auth)
-except HTTPError as http_err:
-    print(f'Http error occurred: {http_err}')
-except Exception as err:
-    print(f'Other error occurred: {err}')
-else:
-    print('Success!')
+# List of all connections used by source integrations
+connections = {}
+
+for integration in integrations['items']:
+    id = (integration['id']).replace('|', '%7C')
+    if integration['status'] == 'ACTIVATED':
+        print(id)
+        try:
+            # exportIntegration(source, id, auth)
+            # importIntegration(target, auth, id + '.iar')
+            
+            
+            for conn in integration['endPoints']:
+                connId = conn['connection']['id']
+                connLink = conn['connection']['links'][0]['href']
+                connections[connId] = connLink
+            
+
+            # activateIntegration(target, auth, id)
+            # deactivateIntegration(source, auth, id)
+            # retrieveConnections(sourceConnections, auth)
+        except HTTPError as http_err:
+            print(f'Http error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+
+for id in connections:
+    # Payload containes connection properties
+    payload = {}
+    # Group of related properties
+    propertiesGroup = []
+
+    for properties in env[id]:
+        for property in env[id][properties]:
+            keyValue = {}
+            keyValue['propertyName'] = property
+            keyValue['propertyValue'] = env[id][properties][property]
+            if properties == 'attachment':
+                try:
+                    # Doesn't work for ATP connections:
+                    # Bug 29701192 : Upload Connection Property Attachment REST API not working for ATP Connection
+                    uploadConnectionPropertyAttachment(targetConnections, auth, id, keyValue['propertyName'], keyValue['propertyValue'])
+                except HTTPError as http_err:
+                    print(f'Http error occurred: {http_err}')
+                    pass
+                except Exception as err:
+                    print(f'Other error occurred: {err}')
+                    pass
+            propertiesGroup.append(keyValue)
+            payload[properties] = propertiesGroup
+            try:
+                updateConnection(targetConnections, auth, id, payload)
+            except HTTPError as http_err:
+                print(f'Http error occurred: {http_err}')
+                pass
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+                pass
+
+        # securityProperties = []
+        # securityProperty = {}
+        # for property in env[id]['securityProperties']:
+        #     securityProperty = {}
+        #     securityProperty['propertyName'] = property
+        #     securityProperty['propertyValue'] = env[id]['securityProperties'][property]
+        #     securityProperties.append(securityProperty)
+        #     payload['securityProperties'] = securityProperties
+
+        # try:
+        #     updateConnection(targetConnections, auth, id, payload)
+        #     for property in env[id]['attachment']:
+        #         uploadConnectionPropertyAttachment(targetConnections, auth, id, property, env[id]['attachment'][property])
+        # except HTTPError as http_err:
+        #     print(f'Http error occurred: {http_err}')
+        #     pass
+        # except Exception as err:
+        #     print(f'Other error occurred: {err}')
+        #     pass
+    # if id == 'ERP_TEST_CONN':
+    #     payload = {}
+    #     connectionProperties = []
+    #     securityProperties = []
+    #     for connectionProperty in connection['connectionProperties']:
+    #         if connectionProperty['propertyName'] == 'targetWSDLURL':
+    #             connectionProperty['propertyValue'] = 'https://ekmt-dev3.fa.us6.oraclecloud.com/fscmService/ServiceCatalogService?WSDL'
+    #             connectionProperties.append(connectionProperty)
+            
+    #     payload['connectionProperties'] = connectionProperties
+
+    #     for securityPolicyInfo in connection['securityPolicyInfo']:
+    #         if securityPolicyInfo['securityPolicy'] == 'USERNAME_PASSWORD_TOKEN':
+    #             payload['securityPolicy'] = 'USERNAME_PASSWORD_TOKEN'
+    #             for securityProperty in securityPolicyInfo['securityProperties']:
+    #                 if securityProperty['propertyName'] == 'username':
+    #                     securityProperty['propertyValue'] = 'integrationuser'
+    #                     securityProperties.append(securityProperty)
+    #                 if securityProperty['propertyName'] == 'password':
+    #                     securityProperty['propertyValue'] = 'Welcome1'
+    #                     securityProperties.append(securityProperty)
+                        
+    #     payload['securityProperties'] = securityProperties
+    #     updateConnection(targetConnections, auth, id, payload)
+
+    # if id == 'ATP_PERF_COMMON':
+    #     payload = {}
+    #     connectionProperties = []
+    #     securityProperties = []
+    #     for connectionProperty in connection['connectionProperties']:
+    #         if connectionProperty['propertyName'] == 'ServiceName':
+    #             connectionProperty['propertyValue'] = 'atpstage_low'
+    #             connectionProperties.append(connectionProperty)
+            
+    #     payload['connectionProperties'] = connectionProperties
+
+    #     for securityPolicyInfo in connection['securityPolicyInfo']:
+    #         if securityPolicyInfo['securityPolicy'] == 'JDBC_OVER_SSL':
+    #             payload['securityPolicy'] = 'JDBC_OVER_SSL'
+    #             for securityProperty in securityPolicyInfo['securityProperties']:
+    #                 if securityProperty['propertyName'] == 'WALLETPASSWORD':
+    #                     securityProperty['propertyValue'] = 'm&7RCzZcK6jLXh'
+    #                     securityProperties.append(securityProperty)
+    #                 if securityProperty['propertyName'] == 'UserName':
+    #                     securityProperty['propertyValue'] = 'airbnb_common'
+    #                     securityProperties.append(securityProperty)
+    #                 if securityProperty['propertyName'] == 'Password':
+    #                     securityProperty['propertyValue'] = 'D55k8qze4Bx3LT49'
+    #                     securityProperties.append(securityProperty)
+                        
+    #     payload['securityProperties'] = securityProperties
+    #     updateConnection(targetConnections, auth, id, payload)
+    #     uploadConnectionPropertyAttachment(targetConnections, auth, id, 'WALLET', 'ATPSTAGE.zip')
 
 

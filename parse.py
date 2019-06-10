04397bcf583f69350a9211867daaa776a3efb16a
kocic11 <25288526+kocic11@users.noreply.github.com>
2019-06-10 17:56:00 -0400

    Implemented single purpose functions

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ from functions import *
 def getActiveIntegrations(url, auth, headers):
     '''Get all active integrations from source.'''
     try:
-        integrations = requests.get(sourceUrl, auth = sourceAuth, headers = headers).json()       
+        integrations = requests.get(url, auth = auth, headers = headers).json()       
         activeIntegrations = {}
         for integration in integrations['items']:
             id = (integration['id']).replace('|', '%7C')
@@ -20,7 +20,7 @@ def getActiveIntegrations(url, auth, headers):
         print(f'Other error occurred: {err}')
         exit
 
-def configureConnections(integrations, env, targetConnectionsUrl, targetAuth):
+def configureConnections(integrations, env, connectionsUrl, auth):
     '''Configure the connections used by source integrations at target'''
     # Get integration connections and add them to the list of connections
     connections = []
@@ -55,10 +55,10 @@ def configureConnections(integrations, env, targetConnectionsUrl, targetAuth):
                         attachment['propertyValue'] = properties[propertyName]
             print(f'Updating connection {id}: {payload}')
             try:
-                updateConnection(targetConnectionsUrl, targetAuth, id, payload)
+                updateConnection(connectionsUrl, auth, id, payload)
                 # Doesn't work for ATP connections:
                 # Bug 29701192 : Upload Connection Property Attachment REST API not working for ATP Connection
-                uploadConnectionPropertyAttachment(targetConnectionsUrl, targetAuth, id, attachment['propertyName'], attachment['propertyValue'])
+                uploadConnectionPropertyAttachment(connectionsUrl, auth, id, attachment['propertyName'], attachment['propertyValue'])
             except HTTPError as http_err:
                 print(f'Http error occurred: {http_err}')
                 pass
@@ -66,26 +66,29 @@ def configureConnections(integrations, env, targetConnectionsUrl, targetAuth):
                 print(f'Other error occurred: {err}')
                 pass
 
-def exportImportIntegrations(integrations, sourceUrl, targetUrl, sourceAuth, targetAuth):
+def exportIntegrations(integrations, url, auth):
     '''Export integrations from source and import them to target'''
     for integration in integrations:
         id = integrations['id']
-        # # Only export/import active integrations
-        # if integration['status'] == 'ACTIVATED':
+        print(f'Export integration: {id}')
+        try:
+            # Export integration
+            exportIntegration(url, id, auth)
+        except HTTPError as http_err:
+            print(f'Http error occurred: {http_err}')
+            pass
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+            pass
+
+def importIntegrations(integrations, url, auth):
+    '''Export integrations from source and import them to target'''
+    for integration in integrations:
+        id = integrations['id']
         print(f'Export/Import integration: {id}')
-        # activeIntegrations.append(id)
         try:
-            # Export integration from source
-            exportIntegration(sourceUrl, id, sourceAuth)
-            
             # Import integration to target
-            importIntegration(targetUrl, targetAuth, id + '.iar')
-
-            # # Get integration connections and add them to the list of connections
-            # for conn in integration['endPoints']:
-            #     connId = conn['connection']['id']
-            #     connLink = conn['connection']['links'][0]['href']
-            #     connections[connId] = connLink
+            importIntegration(url, auth, id + '.iar')
         except HTTPError as http_err:
             print(f'Http error occurred: {http_err}')
             pass
@@ -93,13 +96,13 @@ def exportImportIntegrations(integrations, sourceUrl, targetUrl, sourceAuth, tar
             print(f'Other error occurred: {err}')
             pass
 
-def rectivateIntegrations(integrations, targetUrl, targetAuth):
-    ''' Reactivate imported integrations.'''
+
+def activateIntegrations(integrations, url, auth, tracing):
+    ''' Activate integrations.'''
     for id in integrations:
-        print(f'Reactivate integration {id}')
+        print(f'Activate integration {id}')
         try:
-            deactivateIntegration(targetUrl, targetAuth, id)
-            activateIntegration(targetUrl, targetAuth, id, 'true')
+            activateIntegration(url, auth, id, tracing)
         except HTTPError as http_err:
             print(f'Http error occurred: {http_err}')
             pass
@@ -107,6 +110,20 @@ def rectivateIntegrations(integrations, targetUrl, targetAuth):
             print(f'Other error occurred: {err}')
             pass
 
+def deactivateIntegrations(integrations, url, auth):
+    ''' Deactivate integrations.'''
+    for id in integrations:
+        print(f'Deactivate integration {id}')
+        try:
+            deactivateIntegration(url, auth, id)
+        except HTTPError as http_err:
+            print(f'Http error occurred: {http_err}')
+            pass
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+            pass
+
+# Begin
 lenArgs = len(sys.argv)
 if (lenArgs < 2) :
     print('Usage: parse.py <env.properties>')
@@ -125,8 +142,20 @@ headers = {'Accept' : 'application/json'}
 sourceAuth = (env['sourceUser'], env['sourcePassword'])
 targetAuth = (env['targetUser'], env['targetPassword'])
 
+# Get active configurations
 activeIntegrations = getActiveIntegrations(sourceUrl, sourceAuth, headers)
-# exportImportIntegrations(activeIntegrations, sourceUrl, targetUrl, sourceAuth, targetAuth)
-# configureConnections(activeIntegrations, env, targetConnectionsUrl, targetAuth)
-# rectivateIntegrations(activeIntegrations, targetUrl, targetAuth)
-rectivateIntegrations(activeIntegrations, sourceUrl, targetAuth)
+
+# Export integrations 
+exportIntegrations(activeIntegrations, sourceUrl, sourceAuth)
+
+# Import integrations
+importIntegrations(activeIntegrations, targetUrl, targetAuth)
+
+# COnfigure connections
+configureConnections(activeIntegrations, env, targetConnectionsUrl, targetAuth)
+
+# Deactivate integrations
+deactivateIntegrations(activeIntegrations, sourceUrl, sourceAuth)
+
+# Activate integrations and enable tracing
+activateIntegrations(activeIntegrations, sourceUrl, sourceAuth, 'true')

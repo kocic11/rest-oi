3d8e83cead2668b5f86ebb54e8659bd6c9eed1d7
AKOCIC <AKOCIC@AKOCIC-CA.ca.oracle.com>
2019-06-24 16:32:57 -0400

    Updated on Windows

--- functions.py
+++ functions.py
@@ -1,16 +1,18 @@
 import os, requests, json
+from requests.exceptions import HTTPError
 
-def exportIntegration(baseUrl, id, auth):
+def __exportIntegration(baseUrl, auth, id):
     '''Export an integration with a given ID.'''
     url = baseUrl + '/' + id + '/archive'
     response = requests.get(url, auth = auth, headers = {'Accept' : 'application/octet-stream'})
     if response.status_code == 200:
         fp = open(id + '.iar','wb')
         fp.write(response.content)
+        fp.close()
     else:
         response.raise_for_status()
 
-def importIntegration(baseUrl, auth, file):
+def __importIntegration(baseUrl, auth, file):
     '''Import (add or replace) an integration with the same name that was exported previously.'''
     
     url = baseUrl + '/archive'
@@ -43,19 +45,19 @@ def importIntegration(baseUrl, auth, file):
         print('Server error.')
         response.raise_for_status()
     
-def activateIntegration(baseUrl, auth, id, tracing):
+def __activateIntegration(baseUrl, auth, id, tracing):
     '''Activate integration and enable tracing.'''
     url = baseUrl + '/' + id
     payload = {'status':'ACTIVATED', 'payloadTracingEnabledFlag': tracing}
-    changeStatus(url, auth, payload)
+    __changeStatus(url, auth, payload)
 
-def deactivateIntegration(baseUrl, auth, id):
+def __deactivateIntegration(baseUrl, auth, id):
     '''Deactivate integration.'''
     url = baseUrl + '/' + id
     payload = {'status':'CONFIGURED'}
-    changeStatus(url, auth, payload)
+    __changeStatus(url, auth, payload)
 
-def changeStatus(url, auth, payload):
+def __changeStatus(url, auth, payload):
     '''Change integration status.'''
     response = requests.post(url, auth = auth, headers = {'Content-Type' : 'application/json', 'X-HTTP-Method-Override' : 'PATCH'}, data = json.dumps(payload))
     if response.status_code == 200:
@@ -67,15 +69,38 @@ def changeStatus(url, auth, payload):
         
     response.raise_for_status()
 
-def retrieveConnections(baseUrl, auth):
-    ''' Retrieve a list of connections.'''
-    response = requests.get(baseUrl, auth = auth)
-    if response.status_code == 200:
-        connections = response.json()
-    else:
-        response.raise_for_status()
+def __pauseSchedule(baseUrl, auth, id):
+    '''Pause schedule of an integration flow'''
+    url = baseUrl + '/' + id + '/schedule/pause'
+    response = requests.post(url, auth = auth, headers = {'Content-Type' : 'application/json'})
+    if response.status_code in [200, 412]:
+        print(f'{id} schedule paused')
+        return
+            
+    response.raise_for_status()
 
-def retrieveConnection(url, auth):
+def __resumeSchedule(baseUrl, auth, id):
+    '''Resume schedule of an integration flow'''
+    url = baseUrl + '/' + id + '/schedule/resume'
+    response = requests.post(url, auth = auth, headers = {'Content-Type' : 'application/json'})
+    if response.status_code == 200:
+        print(f'{id} schedule resumed')
+        return
+    if response.status_code == 404:
+        print(f'Resource not found')
+        return
+        
+    response.raise_for_status()
+
+# def retrieveConnections(baseUrl, auth):
+#     ''' Retrieve a list of connections.'''
+#     response = requests.get(baseUrl, auth = auth)
+#     if response.status_code == 200:
+#         connections = response.json()
+#     else:
+#         response.raise_for_status()
+
+def getConnection(url, auth):
     ''' Retrieve a connection.'''
     response = requests.get(url, auth = auth, headers = {'Accept' : 'application/json'})
     if response.status_code == 200:
@@ -83,7 +108,7 @@ def retrieveConnection(url, auth):
     
     response.raise_for_status()
 
-def updateConnection(url, auth, name, payload):
+def __updateConnection(url, auth, name, payload):
     ''' Update a connection.'''
     url = url + '/' + name
     response = requests.post(url, auth = auth, headers = {'X-HTTP-Method-Override' : 'PATCH', 'Content-Type' : 'application/json'}, data = json.dumps(payload))
@@ -92,7 +117,7 @@ def updateConnection(url, auth, name, payload):
     
     response.raise_for_status()
 
-def uploadConnectionPropertyAttachment(url, auth, id, connPropName, file):
+def __uploadConnectionPropertyAttachment(url, auth, id, connPropName, file):
     ''' Upload a connection property attachment.'''
     url = url + '/' + id + '/attachments/' + connPropName
     files = {'file': open(file, 'rb')}
@@ -102,6 +127,31 @@ def uploadConnectionPropertyAttachment(url, auth, id, connPropName, file):
     
     response.raise_for_status()
 
+def __exportLookup(baseUrl, auth, id):
+    '''Export lookup with a given id'''
+    url = baseUrl + '/' + id + '/archive'
+    response = requests.get(url, auth = auth, headers = {'Accept' : 'application/octet-stream'})
+    if response.status_code == 200:
+        print(response.text)
+        fp = open(id + '.csv','w')
+        fp.write(response.text)
+        fp.close()
+    else:
+        response.raise_for_status()
+
+def __importLookup(baseUrl, id, auth):
+    '''Export lookup with a given id'''
+    url = baseUrl + '/' + id + '/archive'
+    response = requests.get(url, auth = auth, headers = {'Accept' : 'application/octet-stream'})
+    if response.status_code == 200:
+        print(response.text)
+        fp = open(id + '.csv','w')
+        fp.write(response.text)
+        fp.close()
+    else:
+        response.raise_for_status()
+
+############ Collections #######################
 def getIntegrations(url, auth, headers, status):
     '''Get integrations based on status.'''
     try:
@@ -119,16 +169,19 @@ def getIntegrations(url, auth, headers, status):
         print(f'Other error occurred: {err}')
         exit
 
-def updateConnections(integrations, env, connectionsUrl, auth):
+def getConnections(integrations, url, auth):
     '''Configure the connections used by source integrations at target'''
     # Get integration connections and add them to the list of connections
     connections = {}
     for id in integrations:
         for connection in integrations[id]['endPoints']:
             connId = connection['connection']['id']
-            link = connection['connection']['links'][0]['href']
-            connections[connId] = link
+            connections[connId] = connection['connection']['links'][0]['href']
+    return connections
+    
 
+def updateConnections(connections, url, auth, env):
+    '''Update connections'''
     for id in connections:
         # Payload containes connection properties
         payload = {}
@@ -153,17 +206,17 @@ def updateConnections(integrations, env, connectionsUrl, auth):
                         attachment['propertyValue'] = properties[propertyName]
                         
             print(f'Updating connection {id}: {payload}')
-            try:
-                updateConnection(connectionsUrl, auth, id, payload)
-                # Doesn't work for ATP connections:
-                # Bug 29701192 : Upload Connection Property Attachment REST API not working for ATP Connection
-                uploadConnectionPropertyAttachment(connectionsUrl, auth, id, attachment['propertyName'], attachment['propertyValue'])
-            except HTTPError as http_err:
-                print(f'Http error occurred: {http_err}')
-                pass
-            except Exception as err:
-                print(f'Other error occurred: {err}')
-                pass
+            # try:
+            #     updateConnection(url, auth, id, payload)
+            #     # Doesn't work for ATP connections:
+            #     # Bug 29701192 : Upload Connection Property Attachment REST API not working for ATP Connection
+            #     uploadConnectionPropertyAttachment(url, auth, id, attachment['propertyName'], attachment['propertyValue'])
+            # except HTTPError as http_err:
+            #     print(f'Http error occurred: {http_err}')
+            #     pass
+            # except Exception as err:
+            #     print(f'Other error occurred: {err}')
+            #     pass
 
 def exportIntegrations(integrations, url, auth):
     '''Export integrations'''
@@ -171,7 +224,7 @@ def exportIntegrations(integrations, url, auth):
         print(f'Export integration: {id}')
         try:
             # Export integration
-            exportIntegration(url, id, auth)
+            __exportIntegration(url, id, auth)
         except HTTPError as http_err:
             print(f'Http error occurred: {http_err}')
             pass
@@ -184,7 +237,7 @@ def importIntegrations(integrations, url, auth):
     for id in integrations:
         print(f'Import integration: {id}')
         try:
-            importIntegration(url, auth, id + '.iar')
+            __importIntegration(url, auth, id + '.iar')
         except HTTPError as http_err:
             print(f'Http error occurred: {http_err}')
             pass
@@ -197,7 +250,7 @@ def activateIntegrations(integrations, url, auth, tracing):
     for id in integrations:
         print(f'Activate integration {id}')
         try:
-            activateIntegration(url, auth, id, tracing)
+            __activateIntegration(url, auth, id, tracing)
         except HTTPError as http_err:
             print(f'Http error occurred: {http_err}')
             pass
@@ -210,10 +263,51 @@ def deactivateIntegrations(integrations, url, auth):
     for id in integrations:
         print(f'Deactivate integration {id}')
         try:
-            deactivateIntegration(url, auth, id)
+            __deactivateIntegration(url, auth, id)
         except HTTPError as http_err:
             print(f'Http error occurred: {http_err}')
             pass
         except Exception as err:
             print(f'Other error occurred: {err}')
             pass
+
+def pauseSchedules(integrations, url, auth):
+    '''Pause integrations'''
+    for id in integrations:
+        print(f'Pause integration {id}')
+        try:
+            __pauseSchedule(url, auth, id)
+        except HTTPError as http_err:
+            print(f'Http error occurred: {http_err}')
+            pass
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+            pass
+
+def exportLookups(lookups, url, auth):
+    '''Export lookups'''
+    for id in lookups:
+        print(f'Export lookup {id}')
+        try:
+            __exportLookup(url, auth,  id)
+        except HTTPError as http_err:
+            print(f'Http error occurred: {http_err}')
+            pass
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+            pass
+
+def getLookups(url, auth):
+    '''Get integrations based on status.'''
+    try:
+        response = requests.get(url, auth = auth).json()       
+        lookups = {}
+        for lookup in response['items']:
+            lookups[lookup['id']] = lookup
+        return  lookups
+    except HTTPError as http_err:
+        print(f'Http error occurred: {http_err}')
+        pass
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        pass
--- parse.py
+++ parse.py
@@ -1,11 +1,16 @@
-import sys, json, os, requests
+import json
+import os
+import sys
+
+import requests
 from requests.exceptions import HTTPError
-from functions import *
+
+from functions import getIntegrations, getLookups, getConnection, getConnections, exportIntegrations, exportLookups
 
 # Begin
 lenArgs = len(sys.argv)
 if (lenArgs < 2) :
-    print('Usage: parse.py <env.properties>')
+    print('Usage: parse.py <env.json>')
     exit()
 
 # Read env.json file
@@ -13,28 +18,69 @@ fp = open(sys.argv[1], 'r')
 env = json.load(fp)
 fp.close()
 
-sourceUrl = env['sourceUrl'] + env['integrations']
-targetUrl = env['targetUrl'] + env['integrations']
+sourceIntegrations = env['sourceUrl'] + env['integrations']
+targetIntegrations = env['targetUrl'] + env['integrations']
 sourceConnections = env['sourceUrl'] + env['connections']
-targetConnectionsUrl = env['targetUrl'] + env['connections']
+targetConnections = env['targetUrl'] + env['connections']
+sourceLookups = env['sourceUrl'] + env['lookups']
+targetLookups = env['targetUrl'] + env['lookups']
 headers = {'Accept' : 'application/json'}
 sourceAuth = (env['sourceUser'], env['sourcePassword'])
 targetAuth = (env['targetUser'], env['targetPassword'])
 
 # Get active configurations
-integrations = getIntegrations(sourceUrl, sourceAuth, headers, 'ACTIVATED')
+integrations = getIntegrations(sourceIntegrations, sourceAuth, headers, 'ACTIVATED')
+
+# Write integrations JSON file
+fp = open('integrations.json','w')
+fp.write(json.dumps(integrations, indent=4))
+fp.close()
+
+lookups = getLookups(sourceLookups, sourceAuth)
+fp = open('lookups.json','w')
+fp.write(json.dumps(lookups, indent=4))
+fp.close()
+
+connections = getConnections(integrations, sourceConnections, sourceAuth)
+fp = open('connections.json','w')
+fp.write(json.dumps(connections, indent=4))
+fp.close()
+
+connection = getConnection(connections['ODI_FILE_PICKUP'], sourceAuth)
+fp = open('ODI_FILE_PICKUP.json','w')
+fp.write(json.dumps(connection, indent=4))
+fp.close()
+
+# exportIntegration(sourceIntegrations, sourceAuth, 'SCHEDULE_FBDI%7C03.20.0000')
+# deactivateIntegration(targetIntegrations, targetAuth, 'SCHEDULE_FBDI%7C03.10.0000')
+# importIntegration(targetIntegrations, targetAuth, 'SCHEDULE_FBDI%7C03.20.0000.iar')
+# activateIntegration(targetIntegrations, targetAuth, 'SCHEDULE_FBDI%7C03.20.0000', 'true')
 
 # Export integrations 
-#exportIntegrations(integrations, sourceUrl, sourceAuth)
+exportIntegrations(integrations, sourceIntegrations, sourceAuth)
+
+# Export lookups 
+exportLookups(lookups, sourceLookups, sourceAuth)
+
+# Import lookups 
+# importLookups(lookups, targLookups, targetAuthAuth)
+
+# Configure connections
+# updateConnections(connections, env, targetConnections, targetAuth)
+
+# # Pause integrations
+# pauseSchedule(sourceIntegrations, sourceAuth, 'SCHEDULE_PAAS_METADATA_REFRESH%7C01.80.0000')
+# pauseSchedule(sourceIntegrations, sourceAuth, 'EXCHANGE_RATES_OLD%7C02.00.0000')
+
+# # Deactivate integrations
+# deactivateIntegrations(integrations, targetIntegrations, targetAuth)
 
 # Import integrations
-#importIntegrations(integrations, targetUrl, targetAuth)
-
-# COnfigure connections
-updateConnections(integrations, env, targetConnectionsUrl, targetAuth)
-
-# Deactivate integrations
-deactivateIntegrations(integrations, sourceUrl, sourceAuth)
+# importIntegrations(integrations, targetIntegrations, targetAuth)
 
 # Activate integrations and enable tracing
-activateIntegrations(integrations, sourceUrl, sourceAuth, 'true')
+# activateIntegrations(integrations, targetIntegrations, targetAuth, 'true')
+
+# # Resume integrations
+# resumeSchedule(sourceIntegrations, sourceAuth, 'SCHEDULE_PAAS_METADATA_REFRESH%7C01.80.0000')
+# resumeSchedule(sourceIntegrations, sourceAuth, 'EXCHANGE_RATES_OLD%7C02.00.0000')
